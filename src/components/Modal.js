b392949dceacb493778fe0536b76bf64be8199e4
EnerioBalungcas <enerio.balungcas@gmail.com>
2022-09-22 09:38:44 +0800

    changes in properties page and component

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -2,20 +2,13 @@ import React, {useState} from 'react';
 import ModalWindow from '../pages/Modalwindow';
 
 function Modal() {
-    const [openModal, setOpenModal] = useState(false);
+    // const [openModal, setOpenModal] = useState(false);
 
   return (
-    <>
-    <div>
-        <h1>Hey, click on the button to open the modal.</h1>
-        <button className="openModalBtn" onClick={()=>{setOpenModal(true)}}>Open</button>
-        {openModal && <ModalWindow closeModal={setOpenModal}/>}
-    </div>
+  
+    <div></div>
 
-    
-    </>
-
-  )
+  );
 }
 
-export default Modal
+export default Modal;
--- src/pages/Properties.js
+++ src/pages/Properties.js
@@ -15,7 +15,12 @@ function Properties() {
             key={key}
             image={propertiesItem.image} 
             name={propertiesItem.name} 
-            price={propertiesItem.price}/>
+            price={Number(propertiesItem.price).toLocaleString('en-US')}
+            region={propertiesItem.region}
+            city={propertiesItem.city}
+            floorarea={propertiesItem.floorarea}
+            lotarea={propertiesItem.lotarea}
+            description={propertiesItem.description}/>
           );
         })}
       </div>
