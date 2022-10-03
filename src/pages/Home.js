518b68c059cce985790a6ea22dc616fd15476547
LomejaneBabao <lomejanebabao37@gmail.com>
2022-10-03 13:06:13 +0800

    added git ignore and modified home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -215,29 +215,5 @@ function Home() {
 }
 
 export default Home;
-import React from 'react'
-import { Link } from 'react-router-dom';
-import BannerImage from '../assets/properties_banner.jpg'
-import '../styles/Home.css'
-function Home() {
-  return (
-    <div className="home" style={{backgroundImage:`url(${BannerImage})`}}> 
-      <div className="headerContainer" >
 
-        {/* <div className="infoContainer">
-          Search: <input type="text" placeholder="Search..."/>
-        </div> */}
-
-        <h1>LEM Properties</h1>
-        <p>Buy, Sell and Rent</p>
-        <Link to='/properties'>
-          <button>RESERVE NOW!!!</button>
-        </Link>
-      </div>
-    </div>
-  );
-  
-}
-
-export default Home
 
